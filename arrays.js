0906693500c8d2dcabb50f139099140ea0a3bf5a
Joel Mosemann <mosemann@hotmail.com>
2017-12-05 17:19:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(a,b) {
+  b.push(a)
+  return a
+}
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  b.unshift(a);
+  return a;
+}
+function addElementToEndOfArray(a,b) {
+  return [. . .a, b]
+}
+function destructivelyAddElementToEndOfArray(a,b) {
+  a = [. . .a, b]
+  return a
+}
+function accessElementInArray(a,b) {
+  return a[b]
+}
+function destructivelyRemoveElementFromBeginningOfArray(a) {
+  a.shift()
+  return a
+}
+function removeElementFromBeginningOfArray(a) {
+  a.slice(1)
+  return a
+}
+function destructivelyRemoveElementFromEndOfArray(a) {
+  a.pop(1)
+  return addElementToBeginningOfArray
+}
+function removeElementFromEndOfArray(a) {
+  
+}
